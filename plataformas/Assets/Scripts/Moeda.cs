b5cebff49e999b0a5fc16f0568b40886613df7e9
LizianeGomes <145374731+LizianeGomes@users.noreply.github.com>
2026-05-21 15:44:12 -0300

    hf

--- plataformas/Assets/Scripts/Moeda.cs
+++ plataformas/Assets/Scripts/Moeda.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class Moeda : MonoBehaviour
+public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("Player"))
         {
-            
+           
             PlayerOM.ColetarMoeda(1);
 
-         
+            
             Destroy(gameObject);
         }
     }
--- plataformas/Assets/Scripts/PlayerOM.cs
+++ plataformas/Assets/Scripts/PlayerOM.cs
@@ -2,10 +2,8 @@ using System;
 
 public static class PlayerOM
 {
-  
     public static Action<int> OnMoedaColetada;
 
-    
     public static void ColetarMoeda(int quantidade)
     {
         OnMoedaColetada?.Invoke(quantidade);
